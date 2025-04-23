f012a8de2c2a367e14e81aae24012ab922328ef1
jason78999 <borzvolk888@proton.me>
2025-04-23 17:51:58 +0200

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white text-center p-4">
+      <h1 className="text-5xl font-bold mb-4">$STATH</h1>
+      <p className="text-xl italic">"Позвоночник знаешь, я позвонил."</p>
+    </div>
+  </React.StrictMode>,
+)
