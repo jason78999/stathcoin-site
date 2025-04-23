358a07c84800a88fc1159b60abc8c821cda5e72e
jason78999 <borzvolk888@proton.me>
2025-04-23 18:51:10 +0200

    Delete src directory

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="flex items-center justify-center h-screen text-4xl font-bold">
-      STATHCOIN — THIS ISN’T A MEME. IT’S A WARNING.
-    </div>
-  );
-}
-
-export default App;
